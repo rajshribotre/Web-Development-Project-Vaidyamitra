233b4b3c52fce47b8f31eb5f31dda4d394cb1096
Rajshri Botre <rajshribotre2004@gmail.com>
2024-09-23 21:33:09 +0530

    first commit

---
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import DoctorCard from './../../components/Doctors/DoctorCard';
+import {doctors} from './../../assets/data/doctors';
+
+const Doctors = () => {
+  return (
+    <>
+     <section className='bg-[#fff9ea]'>
+      <div className='container text-center'>
+        <h2 className='heading'> Find a Doctor</h2>
+      </div>
+
+     </section>
+    </>
+  )
+}
+
+export default Doctors
